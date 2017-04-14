a85925b5e697cacc88d693b767641851e0fd5c7a
Antoha5001 <plantsvzzombi@mail.ru>
2017-04-14 17:04:08 +0700

    end

--- gulpfile.js
+++ gulpfile.js
@@ -14,6 +14,7 @@ var gulp           = require('gulp'),
 		ftp            = require('vinyl-ftp'),
 		notify         = require("gulp-notify"),
 		gcmq         = require("gulp-group-css-media-queries"),
+		smartgrid         = require("smart-grid"),
 		srv 						= 'flexbox.less:82';
 
 // Скрипты проекта
@@ -139,3 +140,41 @@ gulp.task('removedist', function() { return del.sync('dist'); });
 gulp.task('clearcache', function () { return cache.clearAll(); });
 
 gulp.task('default', ['watch']);
+/* It's principal settings in smart grid project */
+var settings = {
+    outputStyle: 'less', /* less || scss || sass || styl */
+    columns: 12, /* number of grid columns */
+    offset: "30px", /* gutter width px || % */
+    container: {
+        maxWidth: '1200px', /* max-width оn very large screen */
+        fields: '30px' /* side fields */
+    },
+    breakPoints: {
+        lg: {
+            'width': '1100px', /* -> @media (max-width: 1100px) */
+            'fields': '30px' /* side fields */
+        },
+        md: {
+            'width': '960px',
+            'fields': '15px'
+        },
+        sm: {
+            'width': '780px',
+            'fields': '15px'
+        },
+        xs: {
+            'width': '560px',
+            'fields': '15px'
+        }
+        /* 
+        We can create any quantity of break points.
+
+        some_name: {
+            some_width: 'Npx',
+            some_offset: 'N(px|%)'
+        }
+        */
+    }
+};
+
+smartgrid('app', settings);
